8eed73427545055d08a46cd9b9b8fd9006b7d96e
BellamyBlake <30295713+BellamyBlake@users.noreply.github.com>
2017-07-19 17:40:45 +0200

    Update clean.py

--- src/scripts/clean.py
+++ src/scripts/clean.py
@@ -1,39 +1,3 @@
- # Greetings from BenSomosi - Pyton 3.x
-# -*- ISO: UTF-8 -*-
-'''
-from math import *
-from tkinter import *
-
-text = "welcome"
-
-
-def welcome () :
-      if text == "welcome":
-         print ("Hello Friend")
-        
-welcome ()
-
-if (text == "welcome") :
-      fw = open('visitors.txt', 'w+')
-      fw.write('a a a 3')
-      fw.close()
-
-      for i in range(1):
-            fr = open('visitors.txt','r+')
-            read = fr.read().split()
-
-            t = int(read[2])
-
-            write = fr.write('a').split()
-
-            fr.close()
-      
-
-else:
-      print ("0x0403 - Forbidden: You don't have permission to access [directory] on this server")
-
-# Version 9.8.23  
-'''
 
 delete = open('log.txt', 'w+')
 delete.write('')
